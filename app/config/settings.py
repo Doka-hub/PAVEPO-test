a660a339ab2cac743512117a8fa046a90308abae
Doka <dokadair@gmail.com>
2025-03-30 17:19:16 +0600

    Add YANDEX creds

--- app/config/settings.py
+++ app/config/settings.py
@@ -7,14 +7,19 @@ class Settings(BaseSettings):
     class ConfigDict:
         env_file = '.env'
 
-    DATABASE_URL: str = 'sqlite+aiosqlite:///./test.db'
+    DATABASE_URL: str
     BASE_URL: str = 'http://localhost:8000/api/v1'
 
+    # JWT
     SECRET_KEY: str
     ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
+    # YANDEX
+    YANDEX_CLIENT_ID: str
+    YANDEX_CLIENT_SECRET: str
+
 
 @lru_cache()
-def get_settings():
-    return Settings()
+def get_settings(env_file: str = '.env'):
+    return Settings(_env_file=env_file)
