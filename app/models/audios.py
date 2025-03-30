806518075ae286c317293830427f0bd223e54498
Doka <dokadair@gmail.com>
2025-03-30 16:06:22 +0600

    Beatify code

--- app/models/audios.py
+++ app/models/audios.py
@@ -6,6 +6,7 @@ from app.config.database import Base
 
 class AudioFile(Base):
     __tablename__ = "audio_files"
+
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     file_name = Column(String)
--- app/models/users.py
+++ app/models/users.py
@@ -6,10 +6,10 @@ from app.config.database import Base
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
     yandex_id = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True, nullable=True)
     full_name = Column(String, nullable=True)
     is_superuser = Column(Boolean, default=False)
     audio_files = relationship("AudioFile", back_populates="owner")
-
