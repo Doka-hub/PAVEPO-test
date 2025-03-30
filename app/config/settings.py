a752f1de8d355475ffa1529a457fdc5a94f7c083
Doka <dokadair@gmail.com>
2025-03-30 16:06:44 +0600

    Add jwt fields

--- app/config/settings.py
+++ app/config/settings.py
@@ -10,6 +10,10 @@ class Settings(BaseSettings):
     DATABASE_URL: str = 'sqlite+aiosqlite:///./test.db'
     BASE_URL: str = 'http://localhost:8000/api/v1'
 
+    SECRET_KEY: str
+    ALGORITHM: str = 'HS256'
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
 
 @lru_cache()
 def get_settings():
