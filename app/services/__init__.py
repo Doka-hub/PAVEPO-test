f1493246ceaeaabd900cbd6bf3bebd6212944c67
Doka <dokadair@gmail.com>
2025-03-30 18:57:57 +0600

    Add update_user, delete_user

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,6 +1,8 @@
-from .auth import create_access_token, get_current_user
+from .auth import create_access_token, get_current_user, update_user, delete_user
 
 __all__ = [
     "create_access_token",
     "get_current_user",
+    "update_user",
+    "delete_user",
 ]
