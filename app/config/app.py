d78d9daca94bcfcb10346c792863e346b609de85
Doka <dokadair@gmail.com>
2025-03-30 18:57:32 +0600

    Include users router

--- app/config/app.py
+++ app/config/app.py
@@ -38,6 +38,7 @@ def create_app(
         redoc_url=f"/api/v1/redoc",
     )
 
-    from app.api import auth
+    from app.api import auth, users
     app.include_router(auth.router, prefix="/api/v1")
+    app.include_router(users.router, prefix="/api/v1")
     return app
